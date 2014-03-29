1319a43300e4e785c5da0697b8cd2a508e97497f
Daniel Greenberg <dgreenbe777@gmail.com>
2014-03-29 16:35:25 -0400

    heroku fix

--- app/assets/javascripts/social_media.js
+++ app/assets/javascripts/social_media.js
@@ -1,7 +1,7 @@
   !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
   !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
 
-  <div id="fb-root"></div>
+  // <div id="fb-root"></div>
   (function(d, s, id) {
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) return;
