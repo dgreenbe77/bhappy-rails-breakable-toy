7fde8c0d803dd7bae3286508ae9aaed079f6c1cb
Daniel Greenberg <dgreenbe777@gmail.com>
2014-04-06 15:56:07 -0400

    asset precompile

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,6 @@ module Bhappy
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.filepicker_rails.api_key = 'A3mPX9tZCS5yxhTZDirmHz'
+    config.serve_static_assets = true
   end
 end
